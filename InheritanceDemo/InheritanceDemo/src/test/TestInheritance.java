2cd2e8dadfa7c4e922d9c3606ed05f29546aaa5e
unknown <erkunalrs@gmail.com>
2024-10-05 18:20:26 +0530

    Inheritance Project

---
+++ InheritanceDemo/InheritanceDemo/src/test/TestInheritance.java
@@ -0,0 +1,27 @@
+package test;
+
+//import com.demo.beans.GraduateStudent;
+//import com.demo.beans.MasterStudent;
+import java.time.LocalDate;
+
+import beans.GraduateStudent;
+import beans.MasterStudent;
+public class TestInheritance {
+public static void main(String[] args) {
+
+		// Student s=new Student(12,"xxx","pune",LocalDate.of(2000, 04, 27),new
+		// int[]{12,34});
+		// System.out.pritlnn(s);
+		GraduateStudent g = new GraduateStudent(12, "xxx", "pune", LocalDate.of(2000, 04, 27), new int[] { 12, 34 },
+				89);
+		System.out.println(g);
+		// static polymorphism
+		System.out.println("Grade: " + g.calculateGrade());
+		MasterStudent m = new MasterStudent(12, "xxx", "pune", LocalDate.of(2000, 04, 27), new int[] { 12, 34 }, "yyyy",
+				"BSC", 89);
+		System.out.println(m);
+		// static polymorphism
+		System.out.println("Grade: " + m.calculateGrade());
+
+	}
+}
